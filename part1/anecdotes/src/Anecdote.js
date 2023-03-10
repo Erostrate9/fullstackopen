8c9ca095decf33253c038d28dc0e860b1985501a
erostrate9 <erostrate9@foxmail.com>
2023-03-10 13:13:01 +0800

    edited folder names in part 0

---
+++ part1/anecdotes/src/Anecdote.js
@@ -0,0 +1,7 @@
+const Anecdote = ({text, votes}) => {
+    return (<div>
+        <p>{text}</p>
+        <p>has {votes} votes</p>
+    </div>)
+}
+export default Anecdote
---
+++ part1/unicafe/src/StatisticLine.js
@@ -0,0 +1,8 @@
+
+const StatisticLine = ({text, value}) => 
+    (<tr>
+        <th>{text}</th>
+        <td>{value}</td>
+    </tr>)
+
+export default StatisticLine
